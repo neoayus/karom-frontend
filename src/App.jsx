ca2f25bb09ac13a7a49a80bc2733dc3ea42a4382
neoayus <ayus43@outlook.com>
2026-03-03 01:09:33 +0530

    feat: update URL for Backend Server

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App(){
     const formData = new FormData(); 
     formData.append("file", file);
     
-    const res = await fetch("http://127.0.0.1:8000/upload", {
+    const res = await fetch("https://yokibashi.onrender.com/upload", {
       method: "POST", 
       body: formData,
     });
@@ -36,7 +36,7 @@ export default function App(){
     console.log("Upload response: ", data);
     
     // save the download url from backend 
-    setDownloadURL(`http://127.0.0.1:8000/download/${data.converted_file}`);
+    setDownloadURL(`https://yokibashi.onrender.com/download/${data.converted_file}`);
   };
   
   function handleDownload(){
